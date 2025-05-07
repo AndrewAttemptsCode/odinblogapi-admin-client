5d24068fd8e1df682955d94e0d27757c658a7579
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-07 15:28:07 +0100

    feat: add an error element to handle unexpected errors

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,11 +3,13 @@ import Layout from "../components/Layout";
 import { PostsPage } from "../pages/PostsPage";
 import { CommentsPage } from "../pages/CommentsPage";
 import { CreatePostPage } from "../pages/CreatePostPage";
+import ErrorPage from "../pages/ErrorPage";
 
 const routes = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Navigate to={'/posts'} replace /> },
       { path: 'posts', element: <PostsPage /> },
