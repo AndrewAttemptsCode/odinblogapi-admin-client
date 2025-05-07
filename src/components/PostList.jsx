8eba938062526ae4be386cf233e17a564baaa280
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-07 18:36:47 +0100

    feat: add component to fetch all posts and display them in a list

---
+++ src/components/PostList.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import PostCard from "./PostCard";
+
+export const PostList = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/posts', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+  
+        if (!response.ok) {
+          const error = new Error('Failed to fetch posts from the server.');
+          error.status = response.status;
+          error.statusText = response.statusText;
+          throw error;
+        }
+  
+        const data = await response.json();
+
+        setPosts(data.posts);
+
+      } catch (error) {
+        if (error.message === 'Failed to fetch') {
+          setError({
+            status: 503,
+            statusText: 'Service Unavailable.',
+            message: 'Could not connect to the server, please try again later.',
+          })
+        } else {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }  
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    throw error;
+  }
+
+  return (
+    <div>
+    {posts.map(post => (
+      <PostCard key={post.id} post={post} />
+    ))}
+    </div>
+  );
+}
