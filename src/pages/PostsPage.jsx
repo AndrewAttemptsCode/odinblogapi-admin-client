9a17d430d1ac3faa657d23693585d1cd579fb654
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-07 18:36:00 +0100

    feat: add page to enclose posts

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,7 +1,10 @@
+import { PostList } from "../components/PostList";
+
 export const PostsPage = () => {
   return (
     <section>
-      posts page here
+      <h1>Manage Posts</h1>
+      <PostList />
     </section>
   );
 };
