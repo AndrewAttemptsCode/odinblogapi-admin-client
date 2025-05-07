ffb94e8e2cfd3e7f32cdebc0bb5dd7d4698c4696
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-07 15:10:04 +0100

    feat: add the page routes as children to the layout route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Layout from "../components/Layout";
+import { PostsPage } from "../pages/PostsPage";
+import { CommentsPage } from "../pages/CommentsPage";
+import { CreatePostPage } from "../pages/CreatePostPage";
 
 const routes = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to={'/posts'} replace /> },
+      { path: 'posts', element: <PostsPage /> },
+      { path: 'comments', element: <CommentsPage /> },
+      { path: 'create-post', element: <CreatePostPage /> },
+    ]
   },
 ]);
 
