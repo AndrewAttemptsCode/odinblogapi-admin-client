72356c660326aa580221025f1680c648d98633c1
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-13 15:56:40 +0100

    feat: add responsive layout styling

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,10 +1,30 @@
+import styled from "styled-components";
 import { PostList } from "../components/PostList";
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+const PageTitle = styled.h1`
+  color: #3B82F6;
+  font-size: 3rem;
+  
+  & span {
+    color: #111827;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 2rem;
+  }
+`
 
 export const PostsPage = () => {
   return (
-    <section>
-      <h1>Manage Posts</h1>
+    <Section>
+      <PageTitle>Manage <span>Posts</span></PageTitle>
       <PostList />
-    </section>
+    </Section>
   );
 };
