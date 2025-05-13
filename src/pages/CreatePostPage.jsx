3fd34314918c882479aeac822cb240e2dedb0a19
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-13 16:37:32 +0100

    styles: add responsive layout and styling

--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -1,10 +1,30 @@
+import styled from "styled-components";
 import PostCreate from "../components/PostCreate";
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+const PageTitle = styled.h1`
+  color: #3B82F6;
+  font-size: 3rem;
+  
+  & span {
+    color: #111827;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 2rem;
+  }
+`
 
 export const CreatePostPage = () => {
   return (
-    <section>
-      <h1>Create New Post</h1>
+    <Section>
+      <PageTitle>Create <span>Post</span></PageTitle>
       <PostCreate />
-    </section>
+    </Section>
   );
 };
