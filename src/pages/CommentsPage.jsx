96285bd7cca788179e18f2bdf0e2945531a61776
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-05-14 15:32:49 +0100

    styles: add responsive layout

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,10 +1,30 @@
+import styled from "styled-components";
 import CommentList from "../components/CommentList";
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+const PageTitle = styled.h1`
+  color: #3B82F6;
+  font-size: 3rem;
+  
+  & span {
+    color: #111827;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 2rem;
+  }
+`
 
 export const CommentsPage = () => {
   return (
-    <section>
-      <h1>Manage Comments</h1>
+    <Section>
+      <PageTitle>Manage <span>Comments</span></PageTitle>
       <CommentList />
-    </section>
+    </Section>
   );
 };
